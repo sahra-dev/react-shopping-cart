ec803d677af3b82899c83a3e531afeb136e2254d
sahra <sahra.tehrani2020@yahoo.com>
2023-07-28 20:52:02 +0330

    better UI

--- src/Providers/CartProviders.js
+++ src/Providers/CartProviders.js
@@ -6,7 +6,7 @@ const CartContextDispatcher = createContext()
 
 const initialState = {
   cart: [],
-  total: 5,
+  total: 0,
 }
 const CartProvider = ({ children }) => {
   const [cart, dispatch] = useReducer(cartReducer, initialState)
